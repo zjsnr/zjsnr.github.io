1cc6203bef35c7850913683c57573d15038296ec
gwy15 <gwy15thu@gmail.com>
2018-08-27 20:53:04 +0800

    增加 new 和 老贼 badge

--- js/ranking.js
+++ js/ranking.js
@@ -10,10 +10,18 @@ $().ready(function() {
             option.pageSize = 10;
             for (let item of option.data) {
                 item.ranking = '<a href="mailto:cs@moefantasy.com?' + 
-                'subject=举报' + item.serverName + '服用户 ' + item.username + ' 疑似使用外挂&' +
-                'body=服务器:' +item.serverName + '%0d%0aUID:' + item.uid + '%0d%0a用户名:' + item.username +
-                '%0d%0a%0d%0a通过查询狗牌，该用户最近每天出征在'+ Math.floor(item.speed / 100) * 100 +'次以上，有使用外挂嫌疑。' + 
-                '希望幻萌可以查一下，如果是外挂请进行处理。%0d%0a%0d%0a祝好。%0d%0a%0d%0a">' + item.ranking + '</a>';
+                    'subject=举报' + item.serverName + '服用户 ' + item.username + ' 疑似使用外挂&' +
+                    'body=服务器:' +item.serverName + '%0d%0aUID:' + item.uid + '%0d%0a用户名:' + item.username +
+                    '%0d%0a%0d%0a通过查询狗牌，该用户最近每天出征在'+ Math.floor(item.speed / 100) * 100 +'次以上，有使用外挂嫌疑。' + 
+                    '希望幻萌可以查一下，如果是外挂请进行处理。%0d%0a%0d%0a祝好。%0d%0a%0d%0a">' + item.ranking + '</a>';
+                let firstTime = new Date(item.firstTime);
+                let now = new Date(Date.now());
+                let passedHours = (now - firstTime) / (1000 * 3600);
+                if (passedHours < 24) { // 24 小时内新上榜
+                    item.username += ' <span class="badge badge-danger">new!</span>';
+                } else if (passedHours >= 5 * 24) { // 出现超过 5 天
+                    item.username += ' <span class="badge badge-danger">老贼!</span>';
+                }
             }
             $('#table').bootstrapTable(option);
         })
