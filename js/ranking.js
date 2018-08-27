eff176509e1e2a99e7b29a34016654e38c0717fb
gwy15 <gwy15thu@gmail.com>
2018-08-27 19:36:39 +0800

    增加一键举报

--- js/ranking.js
+++ js/ranking.js
@@ -6,6 +6,15 @@ $().ready(function() {
                 alert(resp.msg);
                 return;
             }
-            $('#table').bootstrapTable(resp.data);
+            var option = resp.data;
+            option.pageSize = 10;
+            for (let item of option.data) {
+                item.ranking = '<a href="mailto:cs@moefantasy.com?' + 
+                'subject=举报' + item.serverName + '服用户 ' + item.username + ' 疑似使用外挂&' +
+                'body=服务器:' +item.serverName + '%0d%0aUID:' + item.uid + '%0d%0a用户名:' + item.username +
+                '%0d%0a%0d%0a通过查询狗牌，该用户最近每天出征在'+ Math.floor(item.speed / 100) * 100 +'次以上，有使用外挂嫌疑。' + 
+                '希望幻萌可以查一下，如果是外挂请进行处理。%0d%0a%0d%0a祝好。%0d%0a%0d%0a">' + item.ranking + '</a>';
+            }
+            $('#table').bootstrapTable(option);
         })
 })
