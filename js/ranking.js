286021e5af4df655442acde98ac1c40fd3251b29
gwy15 <gwy15thu@gmail.com>
2018-08-28 10:55:38 +0800

    暂时屏蔽发邮件链接

--- js/ranking.js
+++ js/ranking.js
@@ -10,11 +10,11 @@ $().ready(function() {
             console.log(option);
             for (let item of option.data) {
                 // 发送邮件
-                item.ranking = '<a href="mailto:cs@moefantasy.com?' + 
-                    'subject=举报' + item.serverName + '服用户 ' + item.username + ' 疑似使用外挂&' +
-                    'body=服务器:' +item.serverName + '%0d%0aUID:' + item.uid + '%0d%0a用户名:' + item.username +
-                    '%0d%0a%0d%0a通过查询狗牌，该用户最近每天出征在'+ Math.floor(item.speed / 100) * 100 +'次以上，有使用外挂嫌疑。' + 
-                    '希望幻萌可以查一下，如果是外挂请进行处理。%0d%0a%0d%0a祝好。%0d%0a%0d%0a">' + item.ranking + '</a>';
+                // item.ranking = '<a href="mailto:cs@moefantasy.com?' + 
+                //     'subject=举报' + item.serverName + '服用户 ' + item.username + ' 疑似使用外挂&' +
+                //     'body=服务器:' +item.serverName + '%0d%0aUID:' + item.uid + '%0d%0a用户名:' + item.username +
+                //     '%0d%0a%0d%0a通过查询狗牌，该用户最近每天出征在'+ Math.floor(item.speed / 100) * 100 +'次以上，有使用外挂嫌疑。' + 
+                //     '希望幻萌可以查一下，如果是外挂请进行处理。%0d%0a%0d%0a祝好。%0d%0a%0d%0a">' + item.ranking + '</a>';
                 // 计算是否依然活跃
                 let lastActiveTime = new Date(item.lastActiveTime);
                 let now = new Date(Date.now());
