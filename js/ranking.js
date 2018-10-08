8746124897533aefb35204a29485a486b71e10a4
zjsnr <41582687+zjsnr@users.noreply.github.com>
2018-10-08 14:01:22 -0400

    修正cookie的bug

--- js/ranking.js
+++ js/ranking.js
@@ -46,16 +46,17 @@ function genTable(resp) {
 }
 
 $().ready(function() {
-    let cookie = Cookies.get('history');
-    if (cookie) {
-        mail = (cookie.indexOf('AABBABAB') >= 0);
-        console.log(mail);
-        if (!mail) {
-            $('#mail').hide();
-        } else {
-            $('#mail').show();
-        }
+    let cookie = "" + Cookies.get('history');
+    console.log("cookie = " + cookie);
+    
+    mail = (cookie.indexOf('AABBABAB') >= 0);
+    console.log(mail);
+    if (!mail) {
+        $('#mail').hide();
+    } else {
+        $('#mail').show();
     }
+
     
     $.getJSON(
         'https://1596403937898061.cn-beijing.fc.aliyuncs.com/2016-08-15/proxy/zjsnr/query/?ranking=1',
