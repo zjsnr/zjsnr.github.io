7f74677ba434c33249fbceb794fecf924318cc2f
gwy15 <gwy15thu@gmail.com>
2018-10-08 14:01:22 -0400

    修正cookie的bug

--- js/common.js
+++ js/common.js
@@ -1,17 +1,10 @@
-$().ready(function() {
-    if (typeof(Cookies.get('history')) == 'undefined') {
-        Cookies.set('history', '');
-    }
-    console.log(Cookies.get('history'));
-})
-
 $('#mail-A').click(function() {
     console.log(Cookies.get('history') + '->');
-    Cookies.set('history', Cookies.get('history') + 'A');
+    Cookies.set('history', "" + Cookies.get('history') + 'A');
     console.log(Cookies.get('history'));
 });
 $('#mail-B').click(function() {
     console.log(Cookies.get('history') + '->');
-    Cookies.set('history', Cookies.get('history') + 'B');
+    Cookies.set('history', "" + Cookies.get('history') + 'B');
     console.log(Cookies.get('history'));
 });
