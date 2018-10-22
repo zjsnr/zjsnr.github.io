768086afe43d03e7c6a560c8fdad42af34d5a727
fuck hm <fuck hm>
2018-10-22 13:23:08 -0400

    修正 URL 过长无法发送的问题

--- js/ranking.js
+++ js/ranking.js
@@ -8,7 +8,9 @@ function genTable(resp) {
     if (resp.mail || mail) {
         $('#mail').show();
         option.clickToSelect = true;
-        option.columns.unshift({checkbox: true});
+        option.columns.unshift({
+            checkbox: true
+        });
     }
 
     for (let item of option.data) {
@@ -24,32 +26,33 @@ function genTable(resp) {
             // 出征停止在 13 小时内
             item.username += '<span class="badge badge-warning">活跃</span>';
         } else {
-            item.username += '<span class="badge badge-secondary">出征停止</span>';
+            item.username +=
+                '<span class="badge badge-secondary">出征停止</span>';
         }
         // 计算是否是 new 或 老贼
         let firstTime = new Date(item.firstTime * 1000);
         let passedHours = (now - firstTime) / (1000 * 3600);
-        if (passedHours < 24) {  // 24 小时内新上榜
+        if (passedHours < 24) { // 24 小时内新上榜
             item.username += '<span class="badge badge-danger">new!</span>';
-        } else if (passedHours >= 5 * 24) {  // 出现超过 5 天
+        } else if (passedHours >= 5 * 24) { // 出现超过 5 天
             item.username += '<span class="badge badge-danger">老贼!</span>';
         }
         // slice
         let fun = (dt) => {
-            return "" + (dt.getMonth() + 1) + "/" + dt.getDate() + " " + dt.getHours() + ":" + dt.getMinutes();
+            return '' + (dt.getMonth() + 1) + '/' + dt.getDate() + ' ' +
+                dt.getHours() + ':' + dt.getMinutes();
         };
         item.maxT = fun(maxT);
         item.minT = fun(minT);
         item.lenT = item.lenT.toFixed(2);
-
     }
     $('#table').bootstrapTable(option);
 }
 
-$().ready(function() {
-    let cookie = "" + Cookies.get('history');
-    console.log("cookie = " + cookie);
-    
+$().ready(function () {
+    let cookie = '' + Cookies.get('history');
+    console.log('cookie = ' + cookie);
+
     mail = (cookie.indexOf('AABBABAB') >= 0);
     console.log(mail);
     if (!mail) {
@@ -58,22 +61,23 @@ $().ready(function() {
         $('#mail').show();
     }
 
-    
+
     $.getJSON(
         'https://1596403937898061.cn-beijing.fc.aliyuncs.com/2016-08-15/proxy/zjsnr/query/?ranking=1',
         genTable);
 });
 
-$('#mail').click(function() {
+$('#mail').click(function () {
     let selections = $('#table').bootstrapTable('getSelections');
-    console.log(selections);
-    console.log(selections.length);
+    // console.log(selections);
+    // console.log(selections.length);
     if (selections.length == 0) {
         return;
     }
     let mailUrl = 'mailto:cs@moefantasy.com?';
-    mailUrl += 'subject=举报' + selections.length + '名用户疑似使用外挂&';
-    let body = ''
+    mailUrl += 'subject=举报' + selections.length + '名用户疑似使用外挂';
+
+    let body = '';
     selections.forEach(item => {
         let para =
             '用户名: ' + item.username.replace(/<span.*\/span>/g, '') + '\n';
@@ -83,7 +87,25 @@ $('#mail').click(function() {
             Math.floor(item.speed / 100) * 100 + '次以上。\n\n';
         body += para;
     });
-    mailUrl += 'body=' + body;
-    mailUrl = encodeURI(mailUrl);
-    window.location.href = mailUrl;
+    let encodeBody = encodeURI(body);
+
+    if (encodeBody.length <= 1800) {
+        location.href = mailUrl + '&body=' + encodeBody;
+    } else {
+        console.log('URL 过长');
+        try {
+            let inp = document.createElement('textarea');
+            document.body.appendChild(inp);
+            inp.value = body;
+            inp.select();
+            document.execCommand('copy', false);
+            inp.remove();
+            alert('由于长度限制，邮件正文已复制到剪切板，请粘贴到正文发送。');
+            window.location.href = mailUrl;
+        } catch (e) {
+            alert(
+                '正文过长无法直接发送；复制邮件正文失败；请尝试选择少量用户多次发送。');
+            return;
+        }
+    }
 });
