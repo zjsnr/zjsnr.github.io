689fd65487ba34ee5d49ec47a0d893e9d4ae6d63
gwy15 <gwy15thu@gmail.com>
2019-05-04 15:24:50 +0800

    bugfix

--- parse.py
+++ parse.py
@@ -11,15 +11,23 @@ for name in ('三射程', '二射程', '缺船炮序'):
     table = workbook.sheet_by_name(name)
     for i in range(1, table.nrows):  # skip header
         row = table.row_values(i)
-        key = ','.join(row[0:6])
-        value = ','.join(
-            str(int(item)) if item else ''
-            for item in row[6:12])
+
+        shootingRange = row[0:6]
+        ranking = [(int(item) if item else 0) for item in row[6:12]]
+        shipRanking = [0 for _ in range(6)]
+        for round, shipIndex in enumerate(ranking):
+            if shipIndex == 0:
+                continue
+            shipRanking[shipIndex - 1] = round + 1
+
+        key = ','.join(shootingRange)
+        value = ','.join((str(item) if item else '') for item in shipRanking)
+
         if key == (',' * 5):
             continue
         if key in data:
             raise RuntimeError(f'key {key} in data.')
         data[key] = value
 
-with open('sequence.json', 'w', encoding='utf8') as f:
+with open('resources/sequence.json', 'w', encoding='utf8') as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
