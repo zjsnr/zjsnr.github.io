d188f4a83252e868b37fe9a74df652aa1652dc34
gwy15 <gwy15thu@gmail.com>
2019-05-05 19:18:02 +0800

    Verify data

--- src/parseShotRankXls.py
+++ src/parseShotRankXls.py
@@ -1,10 +1,11 @@
 import json
 import xlrd
+from collections import Counter
 
 data = {}
 
 # Xls: https://bbs.nga.cn/read.php?tid=17127421
-workbook = xlrd.open_workbook('炮序.xls')
+workbook = xlrd.open_workbook('../data/shootingRank.xls')
 
 # 三射程、二射程
 for name in ('三射程', '二射程', '缺船炮序'):
@@ -14,20 +15,28 @@ for name in ('三射程', '二射程', '缺船炮序'):
 
         shootingRange = row[0:6]
         shipIndexes = [(int(item) if item else 0) for item in row[6:12]]
-        shipRanking = [0 for _ in range(6)]
+        shipRankings = [0 for _ in range(6)]
+
+        counter = Counter(shipIndexes)
+        for index in range(1, max(shipIndexes) + 1):
+            if counter[index] != 1:
+                print(counter)
+                raise RuntimeError(f'False data: Shooting range {shootingRange}, shipIndexes {shipIndexes}')
+
+        # Adjust from shipIndexes to shipRankings
         for round, shipIndex in enumerate(shipIndexes):
             if shipIndex == 0:
                 break
-            shipRanking[shipIndex - 1] = round + 1
+            shipRankings[shipIndex - 1] = round + 1
 
         key = ','.join(shootingRange)
-        value = ','.join((str(item) if item else '') for item in shipRanking)
+        value = ','.join((str(item) if item else '') for item in shipRankings)
 
-        if key == (',' * 5): # empty line
+        if key == (',' * 5):  # empty line
             continue
         if key in data:
-            raise RuntimeError(f'key {key} in data.')
+            raise RuntimeError(f'Duplicate key {key}.')
         data[key] = value
 
-with open('resources/sequence.json', 'w', encoding='utf8') as f:
+with open('../resources/sequence.json', 'w', encoding='utf8') as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
