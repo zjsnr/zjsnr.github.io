961ebc2b119ce1e462357bb3dcddb1bf42f42dfe
gwy15 <gwy15thu@gmail.com>
2019-05-05 19:40:35 +0800

    Add more verification, bugfix

--- src/parseShotRankXls.py
+++ src/parseShotRankXls.py
@@ -7,21 +7,46 @@ data = {}
 # Xls: https://bbs.nga.cn/read.php?tid=17127421
 workbook = xlrd.open_workbook('../data/shootingRank.xls')
 
+
+def verify(shootingRanges, shipIndexes):
+    # Each index should be counted once and only once.
+    counter = Counter(shipIndexes)
+    for index in range(1, max(shipIndexes) + 1):
+        if counter[index] != 1:
+            raise RuntimeError(
+                f'False data: Shooting range {shootingRanges}, shipIndexes {shipIndexes}')
+
+    # assert range descends
+    DEFAULT_RANGE = 5
+    rangeDict = {
+        '超长': 4,
+        '长': 3,
+        '短': 1,
+    }
+    lastRange = DEFAULT_RANGE
+    for shipIndex in shipIndexes:
+        if shipIndex == 0:
+            continue
+        _range = shootingRanges[shipIndex - 1]
+        curRange = rangeDict.get(_range, DEFAULT_RANGE)
+        if curRange > lastRange:
+            raise RuntimeError(
+                f'False data: Shooting range {shootingRanges}, shipIndexes {shipIndexes}')
+        lastRange = curRange
+
+
 # 三射程、二射程
 for name in ('三射程', '二射程', '缺船炮序'):
     table = workbook.sheet_by_name(name)
     for i in range(1, table.nrows):  # skip header
         row = table.row_values(i)
 
-        shootingRange = row[0:6]
+        shootingRanges = row[0:6]
         shipIndexes = [(int(item) if item else 0) for item in row[6:12]]
         shipRankings = [0 for _ in range(6)]
 
-        counter = Counter(shipIndexes)
-        for index in range(1, max(shipIndexes) + 1):
-            if counter[index] != 1:
-                print(counter)
-                raise RuntimeError(f'False data: Shooting range {shootingRange}, shipIndexes {shipIndexes}')
+        # verify data
+        verify(shootingRanges, shipIndexes)
 
         # Adjust from shipIndexes to shipRankings
         for round, shipIndex in enumerate(shipIndexes):
@@ -29,7 +54,7 @@ for name in ('三射程', '二射程', '缺船炮序'):
                 break
             shipRankings[shipIndex - 1] = round + 1
 
-        key = ','.join(shootingRange)
+        key = ','.join(shootingRanges)
         value = ','.join((str(item) if item else '') for item in shipRankings)
 
         if key == (',' * 5):  # empty line
